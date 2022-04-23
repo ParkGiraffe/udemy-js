6c65c4da400cfaf2a53ba1f3909a98b24a032ce5
ParkGiraffe <op5321@soongsil.ac.kr>
2022-04-23 13:41:40 +0900

    Feat: Example 2 practice complete

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -1,4 +1,32 @@
-let js = 'js';
-if (js === 'js') alert('js is amazing!');
+// [2-9] Linking a JavaScript File
+// let js = 'js';
+// if (js === 'js') alert('js is amazing!');
 
-console.log(34+23+435);
+// console.log(34+23+435);
+
+// [2-10] Value and Variable
+// 변수 선언 법
+// let firstName = “Park”;
+
+
+// 변수 선언 규칙 (이름)
+// 변수 이름에 : lowerCamelCase 사용
+// ‘_’도 있지만 JS에서는 camelCase가 표준.
+// 변수 이름을 대문자로 시작하는 것은 오류가 없으나, 컨벤션에 어긋난다.
+
+// 변수 이름은 숫자로 시작할 수 없다.
+// 그리고 오로지 포함 가능한 문자는 numbers, letters, underscores and the dollar sign.
+// let park&yo = ‘Park’; //error
+
+// 예약어(ex: new, function)도 사용 불가능하다.
+
+//  + 변수 말고 상수에는 모든 문자를 대문자로 적는다. ex: const PI = 3.14
+
+// 변수 이름을 지정할 때는 그 값이 어떤 것을 나타내는 지 쉽게 알 수 있도록 정해야 한다.
+// let myFirstJob = 'Programmar';
+// let myCurrentJob = 'Teacher';
+
+// // 아래는 별로인 이름
+// let job1 = 'Programmar';
+// let job2 = 'Teacher';
+
