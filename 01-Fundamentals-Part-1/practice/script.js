9efe63a5f0842569cf4deced0dcc105158a05ffa
ParkGiraffe <op5321@soongsil.ac.kr>
2022-04-24 10:57:47 +0900

    Feat: 2-12 practice complete

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -9,7 +9,7 @@
 // let firstName = “Park”;
 
 
-// 변수 선언 규칙 (이름)
+// 변수 선언 규칙 (이름) Variable name conventions
 // 변수 이름에 : lowerCamelCase 사용
 // ‘_’도 있지만 JS에서는 camelCase가 표준.
 // 변수 이름을 대문자로 시작하는 것은 오류가 없으나, 컨벤션에 어긋난다.
@@ -31,3 +31,27 @@
 // let job2 = 'Teacher';
 //
 
+// // [2-12] Data Types
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun); // true
+
+// console.log(typeof true); // boolean
+// console.log(typeof javascriptIsFun); // boolean
+// console.log(typeof 23); // number
+// console.log(typeof 'joseph'); // string
+
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun); // YES!
+
+
+// // undefined
+// let year;
+// console.log(year); // undefined
+// console.log(typeof year); // undefined
+
+// year = 1991;
+// console.log(year); // 1991
+// console.log(typeof year); // number
+
+// // null
+// console.log(typeof null); // object <- 아무 의미를 지니지 않는다. typeof에서 null일 경우 null이라고 출력하지 않고 object로 출력된다. 따라서 JS에서 버그나 에러로 간주한다. (legacy한 이유로 현재도 수정되지는 않는 버그이다. typeof사용에 유의해야 할 필요가 있는 정보이다.)
