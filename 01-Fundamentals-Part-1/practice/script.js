3dcbd5c85778031dc91f73341cf4098d9f88e5cf
ParkGiraffe <op5321@soongsil.ac.kr>
2022-05-03 15:54:46 +0900

    Feat: 2-29 practice complete

--- 01-Fundamentals-Part-1/practice/script.js
+++ 01-Fundamentals-Part-1/practice/script.js
@@ -539,16 +539,49 @@ GOOD LUCK
 
 
 
-// [2-28] The Conditional (Ternary) Operator
-// 삼항연산자 ? 참일 때 : 거짓일 때
-const age = 23;
-age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+// // [2-28] The Conditional (Ternary) Operator
+// // 삼항연산자 ? 참일 때 : 거짓일 때
+// const age = 23;
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
 
-// Operator는 값을 만든다. 따라서 expression에 속한다. -> 변수에 담을 수 있다.
-const drink = age >= 18? 'wine' : 'water';
-console.log(drink); // wine
+// // Operator는 값을 만든다. 따라서 expression에 속한다. -> 변수에 담을 수 있다.
+// const drink = age >= 18? 'wine' : 'water';
+// console.log(drink); // wine
 
-// 또, expressions이기 때문에 if/else statement와는 다르게 삼항연산자는 template literal에 사용할 수 있다.
-console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+// // 또, expressions이기 때문에 if/else statement와는 다르게 삼항연산자는 template literal에 사용g할 수 있다.
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 
-// 일반적으로 긴 조건문일 때는 if/else 문을 사용하나, 짧은 구문일 때나 JS의 expressions를 필요로 하는 부분(ex: template literal)에서는 삼항 연산자를 사용한다.
+// // 일반적으로 긴 조건문일 때는 if/else 문을 사용하나, 짧은 구문일 때나 JS의 expressions를 필요로 하는 부분(ex: template literal)에서는 삼항 연산자를 사용한다.
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+/* [2-29] Coding Challenge #4
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement � (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 �
+GOOD LUCK �
+*/
+
+const bill = 275
+const tip = bill >= 50 && bill <= 300 ? bill * (15/100) : bill * (20/100);
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+///////////////////////////////////////////////////////////////
