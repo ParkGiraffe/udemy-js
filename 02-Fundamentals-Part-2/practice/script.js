f801540156ee76bcd9b46fad0c6b21b0d1cb6b43
ParkGiraffe <op5321@soongsil.ac.kr>
2022-05-04 16:09:33 +0900

    Feat: 3-35 practice complete

--- 02-Fundamentals-Part-2/practice/script.js
+++ 02-Fundamentals-Part-2/practice/script.js
@@ -63,44 +63,77 @@
 
 
 
-// [3-34] Functions Declarations vs Expressions
-// return에 반환하고자 하는 expression을 적는다.
+// // [3-34] Functions Declarations vs Expressions
+// // return에 반환하고자 하는 expression을 적는다.
 
-// Function declaration
-function calcAge1(birthYear) {
-    return 2022 - birthYear;
+// // Function declaration
+// function calcAge1(birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// const age1 = calcAge1(2000);
+// console.log(age1); // 22
+
+
+// // anonymous function.
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+// const age2 = calcAge2(2000);
+// console.log(age1, age2); // 22 22 
+// // function이 expression이 되어서 calcAge2에 저장된다. 그리고 calcAge2는 함수가 된다.
+// // JS에서 function은 value가 된다. 물론 타입은 없다. string, number와 같은 타입은 없지만 value가 된다.
+
+
+// // Functions Declarations vs Expressions의 차이 : Expressions의 경우 무조건 함수가 먼저 선언된 후에 call 할 수 있다.
+
+// /*
+// const age1 = calcAge1(2000);
+// function calcAge1(birthYear) {
+//     return 2022 - birthYear;
+// }
+
+// console.log(age1); // 22 - OK
+
+// const age2 = calcAge2(2000);
+// const calcAge2 = function (birthYear) {
+//     return 2022 - birthYear;
+// }
+// console.log(age1, age2); // ReferenceError
+// */
+
+// // JS 개발자들은 각각 선호에 따라 두 방법을 혼용해서 사용한다. 그래서 두 방법에 대해 잘 숙지하고 있어야만 한다.
+
+
+
+
+
+
+
+// [2-35] Arrow Functions
+
+// Arrow Function
+// Arrow Function은 Function Exrpessions의 special form이다.
+
+// 한 줄 함수일 경우 : parameter => return할 Exrpession
+const calcAge3 = birthYear => 2022 - birthYear;
+
+// 여러 줄의 코드를 필요로 하는 경우 : {}를 삽입하고 그 안에 return 키워드를 사용한다.
+const yearsUntilRetirement = birthYear => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
 }
 
-const age1 = calcAge1(2000);
-console.log(age1); // 22
-
-
-// anonymous function.
-// Function expression
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear;
+// 여러 개의 parameter를 필요로 하는 경우 : parameter를 적는 부분에 ()를 넣고, 여러 개의 parameters를 적는다.
+const yearsUntilRetirement2 = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
 }
-const age2 = calcAge2(2000);
-console.log(age1, age2); // 22 22 
-// function이 expression이 되어서 calcAge2에 저장된다. 그리고 calcAge2는 함수가 된다.
-// JS에서 function은 value가 된다. 물론 타입은 없다. string, number와 같은 타입은 없지만 value가 된다.
+console.log(yearsUntilRetirement2(2000, 'joseph'));
 
 
-// Functions Declarations vs Expressions의 차이 : Expressions의 경우 무조건 함수가 먼저 선언된 후에 call 할 수 있다.
-
-/*
-const age1 = calcAge1(2000);
-function calcAge1(birthYear) {
-    return 2022 - birthYear;
-}
-
-console.log(age1); // 22 - OK
-
-const age2 = calcAge2(2000);
-const calcAge2 = function (birthYear) {
-    return 2022 - birthYear;
-}
-console.log(age1, age2); // ReferenceError
-*/
-
-// JS 개발자들은 각각 선호에 따라 두 방법을 혼용해서 사용한다. 그래서 두 방법에 대해 잘 숙지하고 있어야만 한다.
+// arrow function이 간단하고 좋긴 하지만, 그렇다고 arrow function만 쓸 수 있는 건 아니다. 대표적으로 'this'키워드를 사용할 수 없다.
