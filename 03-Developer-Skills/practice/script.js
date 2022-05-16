1282b0c27bf0b56c9721d9455a640f239661f93a
ParkGiraffe <op5321@soongsil.ac.kr>
2022-05-16 16:21:55 +0900

    Feat: 5-62 practice complete

--- 03-Developer-Skills/practice/script.js
+++ 03-Developer-Skills/practice/script.js
@@ -67,25 +67,65 @@
 
 // console.log(newAmplitude);
 
-// [5-61] Debugging with the Console and Breakpoints
 
-// Using a debugger
-// JS는 'debugger'키워드가 있으면, 브라우저가 즉시 디버깅 모드를 작동함과 동시에 그 자리에 브레이크 포인트를 설정한다.
-const calcTemAmplitudeBug = function (tp1, tp2) {
-  const temps = tp1.concat(tp2);
-  let min = 0; // bug
-  let max = 0; // bug
-  for (let i = 0; i < temps.length; i++) {
-    const currTemp = temps[i];
-    if (typeof currTemp === 'string');
 
-    debugger;
-    if (currTemp > max) max = currTemp;
-    if (currTemp < min) min = currTemp;
-  }
 
-  return max - min;
-};
-const amplitudeBug = calcTemAmplitudeBug([3, 5, 1], [9, 4, 5]);
 
-console.log(amplitudeBug);
+
+
+// // [5-61] Debugging with the Console and Breakpoints
+
+// // Using a debugger
+// // JS는 'debugger'키워드가 있으면, 브라우저가 즉시 디버깅 모드를 작동함과 동시에 그 자리에 브레이크 포인트를 설정한다.
+// const calcTemAmplitudeBug = function (tp1, tp2) {
+//   const temps = tp1.concat(tp2);
+//   let min = 0; // bug
+//   let max = 0; // bug
+//   for (let i = 0; i < temps.length; i++) {
+//     const currTemp = temps[i];
+//     if (typeof currTemp === 'string');
+
+//     debugger;
+//     if (currTemp > max) max = currTemp;
+//     if (currTemp < min) min = currTemp;
+//   }
+
+//   return max - min;
+// };
+// const amplitudeBug = calcTemAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+// console.log(amplitudeBug);
+
+
+
+/////////////////////////////////////////////////
+/* [5-62] Coding Challenge #1
+Given an array of forecasted maximum temperatures, the thermometer displays a
+string with the given temperatures. Example: [17, 21, 23] will print "... 17ºC in 1
+days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+Your tasks:
+1. Create a function 'printForecast' which takes in an array 'arr' and logs a
+string like the above to the console. Try it with both test datasets.
+2. Use the problem-solving framework: Understand the problem and break it up
+into sub-problems!
+Test data:
+§ Data 1: [17, 21, 23]
+§ Data 2: [12, 5, -5, 0, 4]
+GOOD LUCK �
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str = str + `${arr[i]} in ${i+1} days ... `;
+    }
+    return str
+}
+
+
+console.log(printForecast(data1));
+
+/////////////////////////////////////////////////
