8b70dcdfa8a674829440c2b56dae9b1db590e53b
ParkGiraffe <op5321@soongsil.ac.kr>
2022-05-19 15:50:18 +0900

    Feat: 7-79 practice complete

--- 05-Guess-My-Number/practice/script.js
+++ 05-Guess-My-Number/practice/script.js
@@ -17,7 +17,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-let highScore;
+let highscore;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -31,9 +31,9 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor ='#60b347';
         document.querySelector('.number').style.width = '30rem';
 
-        if (highScore < score) {
-            highScore = score;
-            document.querySelector('highscore').textContent = highScore;
+        if (highscore < score) {
+            highscore = score;
+            document.querySelector('highscore').textContent = highscore;
         }
         
     } else if (guess < secretNumber) {
@@ -53,7 +53,7 @@ document.querySelector('.check').addEventListener('click', function() {
         } else {
             document.querySelector('.message').textContent = 'You lost the game!';
             document.querySelector('.score').textContent = 0;
-        }
+        } 
     }
 });
 
