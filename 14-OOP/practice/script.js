d992eaaac14de7222579e5d846ebe65bd0af1c26
ParkGiraffe <op5321@soongsil.ac.kr>
2022-07-21 14:00:46 +0900

    Study: 14-211 practice complete

--- 14-OOP/practice/script.js
+++ 14-OOP/practice/script.js
@@ -63,3 +63,53 @@ console.log(jonas.species, matilda.species); // Homo Sapiens Homo Sapiens
 
 console.log(jonas.hasOwnProperty('firstName')); // true
 console.log(jonas.hasOwnProperty('species')); // false
+
+
+// [14-211] Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__); // null
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__); // Array.prototype -> Array 내장함수들
+console.log(arr.__proto__ === Array.prototype); // true
+
+console.log(arr.__proto__.__proto__); // object.prototype -> array는 object 타입
+
+// 프로토타입 확장
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+}; 
+// 기존 프로토타입에 직접 메소드를 추가할 수 있다. 예를 들면, 나만의 내장함수를 만들어 모든 array prototype에 뿌릴 수 있다.
+// 하지만 협업에서는 좋은 습관이 아니다. 개발자들이 해당 프로토타입 메소드의 존재를 모를 수 있고, 서로 따로 구현하고 있는 상황이 발생할 수 있다.  또, JS가 다음 버전에서 똑같은 이름의 메소드를 추가할 수 있다.
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1); 
+// 모든 DOM 요소는 스크린 뒤에 작동하는 객체들이다. 
+// h1 -> HTMLHeadingElement -> HTMLElement -> Element -> Node -> EventTarget -> object로 이어지는 prototype 체인으로 구성되어 있다.
+
+console.dir(x => x + 1); 
+// 함수도 객체형이다보니 prototype이 있다. 그래서 함수에서도 내장함수를 쓸 수 있는 것이다.
+
+
+
+// [14-212] Coding Challenge #1
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+};
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+};
+
+const bmw = new Car('BMW', 120);
