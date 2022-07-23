121004ec85e153560fee2a066af97c019b6c12c5
ParkGiraffe <op5321@soongsil.ac.kr>
2022-07-23 17:17:50 +0900

    Study: 14-219 practice complete

--- 14-OOP/practice/script.js
+++ 14-OOP/practice/script.js
@@ -329,3 +329,42 @@ console.log(mike instanceof Object); // true
 // Student.prototype.constructor = Student;
 
 console.dir(Student.prototype.constructor); // Person()
+
+
+
+// [14-219] Coding Challenge #3
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+
+const EV = function(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+EV.prototype.chargeBattery = function(chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function() {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}%`);
+};
+
+const Tesla = new EV('Tesla', 120, 23);
+Tesla.chargeBattery(90);
+Tesla.accelerate();
