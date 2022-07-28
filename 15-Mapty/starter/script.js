2abc38f07de5e0586ea22169f57b01ed0ca0e0cd
ParkGiraffe <op5321@soongsil.ac.kr>
2022-07-28 09:48:38 +0900

    Study: 15-232 practice complete

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation)
+navigator.geolocation.getCurrentPosition(function(position) {
+    // 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    
+}, function() {
+    alert('Could not get your postion');
+});
+// getCurrentPosition()
+// successCallback : 위치를 불러오는 데 성공했을 경우의 callback. 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
+// errorCallback : 위치를 불러오는 데 실팼을 경우의 callback
