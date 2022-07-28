18f406f9b9f4731c65f6ce346834dacd0f332239
ParkGiraffe <op5321@soongsil.ac.kr>
2022-07-28 11:41:28 +0900

    Study: 15-235 practice complete

--- 15-Mapty/practice/script.js
+++ 15-Mapty/practice/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapE;
+
 if (navigator.geolocation)
     // getCurrentPosition() 
     // successCallback : 위치를 불러오는 데 성공했을 경우의 callback. 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
@@ -22,7 +24,7 @@ if (navigator.geolocation)
             const { longitude } = position.coords;
             const coords = [latitude, longitude];
 
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
             // L은 Leaflet의 namespace.
             // map()은 인자로 받은 String을 id로 하는 div에 map 이미지를 불러와주는 메소드
             // setView()는 위도와 경도 배열, 지도 줌 정도를 인자로 받는다.
@@ -34,27 +36,39 @@ if (navigator.geolocation)
             // tileLayer()는 지도의 이미지를 타일 단위로 불러와주는데, openstreetmap 뿐만 아니라 구글맵과 같은 다양한 지도를 불러와준다.
 
             map.on('click', function (mapEvent) {
-                console.log(mapEvent);
-                const { lat, lng } = mapEvent.latlng;
-
-                L.marker([lat, lng])
-                    .addTo(map)
-                    .bindPopup(L.popup({
-                        maxWidth: 250,
-                        minWidth: 100,
-                        autoClose: false,
-                        closeOnClick: false,
-                        className: 'running-popup',
-                    }))
-                    .setPopupContent('workout')
-                    .openPopup();
-
+                mapE = mapEvent;
+                form.classList.remove('hidden');
+                inputDistance.focus();
             })
-
-
-
         },
         function () {
             alert('Could not get your postion');
         }
     );
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Clear input fields
+
+
+    console.log(mapE);
+    const { lat, lng } = mapE.latlng;
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+        }))
+        .setPopupContent('workout')
+        .openPopup();
+});
+
+inputType.addEventListener('change', function() {
+    inputElevation.closest('form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('form__row').classList.toggle('form__row--hidden');
+});
