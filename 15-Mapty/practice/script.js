3c2655ebc42fe129da87730118e2028881f8abf1
ParkGiraffe <op5321@soongsil.ac.kr>
2022-07-28 16:23:40 +0900

    Study: 15-237 practice complete

--- 15-Mapty/practice/script.js
+++ 15-Mapty/practice/script.js
@@ -11,64 +11,85 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapE;
+class App {
+    #map;
+    #mapE;
 
-if (navigator.geolocation)
-    // getCurrentPosition() 
-    // successCallback : 위치를 불러오는 데 성공했을 경우의 callback. 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
-    // errorCallback : 위치를 불러오는 데 실팼을 경우의 callback
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            // 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
-            const { latitude } = position.coords;
-            const { longitude } = position.coords;
-            const coords = [latitude, longitude];
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
 
-            map = L.map('map').setView(coords, 13);
-            // L은 Leaflet의 namespace.
-            // map()은 인자로 받은 String을 id로 하는 div에 map 이미지를 불러와주는 메소드
-            // setView()는 위도와 경도 배열, 지도 줌 정도를 인자로 받는다.
+    _getPosition() {
+        // getCurrentPosition()
+        // successCallback : 위치를 불러오는 데 성공했을 경우의 callback. 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
+        // errorCallback : 위치를 불러오는 데 실팼을 경우의 callback
+        navigator.geolocation.getCurrentPosition(
+            this._loadMap.bind(this),
+            function () {
+                alert('Could not get your postion');
+            }
+        );
+    }
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution:
-                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-            }).addTo(map);
-            // tileLayer()는 지도의 이미지를 타일 단위로 불러와주는데, openstreetmap 뿐만 아니라 구글맵과 같은 다양한 지도를 불러와준다.
+    _loadMap(position) {
+        // 위치를 얻는 데 성공했을 경우 인자로 받아올 수 있다.
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        const coords = [latitude, longitude];
 
-            map.on('click', function (mapEvent) {
-                mapE = mapEvent;
-                form.classList.remove('hidden');
-                inputDistance.focus();
-            })
-        },
-        function () {
-            alert('Could not get your postion');
-        }
-    );
+        this.#map = L.map('map').setView(coords, 13);
+        // L은 Leaflet의 namespace.
+        // map()은 인자로 받은 String을 id로 하는 div에 map 이미지를 불러와주는 메소드
+        // setView()는 위도와 경도 배열, 지도 줌 정도를 인자로 받는다.
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.#map);
+        // tileLayer()는 지도의 이미지를 타일 단위로 불러와주는데, openstreetmap 뿐만 아니라 구글맵과 같은 다양한 지도를 불러와준다.
 
-    // Clear input fields
+        // Handling clicks on map
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapEvent) {
+        this.#mapE = mapEvent;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
 
 
-    console.log(mapE);
-    const { lat, lng } = mapE.latlng;
+    _toggleElevationField() {
+        inputElevation.closest('form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('form__row').classList.toggle('form__row--hidden');
+    }
 
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-        }))
-        .setPopupContent('workout')
-        .openPopup();
-});
+    _newWorkout(e) {
+        e.preventDefault();
 
-inputType.addEventListener('change', function() {
-    inputElevation.closest('form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('form__row').classList.toggle('form__row--hidden');
-});
+        // Clear input fields
+        
+
+        // console.log(this.#mapE);
+        const { lat, lng } = this.#mapE.latlng;
+
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup',
+                })
+            )
+            .setPopupContent('workout')
+            .openPopup();
+    }
+}
+
+
+const app = new App();
