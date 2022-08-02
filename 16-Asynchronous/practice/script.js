7e91bdf3562bd9716c55ff0b70ae30566385df9c
ParkGiraffe <op5321@soongsil.ac.kr>
2022-08-02 10:46:12 +0900

    Study: 15-252 practice complete

--- 16-Asynchronous/practice/script.js
+++ 16-Asynchronous/practice/script.js
@@ -4,9 +4,8 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-// new URL of the API 
+// new URL of the API
 // https://restcountries.com/v2/
-
 
 /*
 // 과거의 방식으로 AJAX 사용해보기. - XMLHttpRequest()
@@ -97,3 +96,34 @@ const getCountryAndNeighbour = function (country) {
     }, 1000);
   }, 1000);
   */
+
+// [15-251] Promises and the Fetch API
+// [15-252] Consuming Promises
+// fetch(url) : url로부터 데이터를 얻어온다. promise를 반환한다.
+// promise.prototype.then(함수) : promise가 fulfilled 상태일 때 작동. then 안의 함수는 fulfilled promise의 결과값을 인자로 받을 수 있다. (이 인자를 response라고 많이 표기함. AJAX 용어)
+// fetch()를 통해 가져온 데이터는 바로 읽을 수는 없는 ReadableStream형식으로 전달된다. 이를 해결하는 방법으로 '.json()' 메소드를 이용하는 것인데, json()또한 async함수라서 또 promise를 반환한다. 그래서 또 then() 메소드로 처리를 할 필요가 있다.
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
