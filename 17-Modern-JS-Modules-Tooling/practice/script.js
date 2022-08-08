fe8aa6d28c82bf3f0051248bece593fe0585610e
ParkGiraffe <op5321@soongsil.ac.kr>
2022-08-08 22:42:07 +0900

    Study: 17-273 practice complete

--- 17-Modern-JS-Modules-Tooling/practice/script.js
+++ 17-Modern-JS-Modules-Tooling/practice/script.js
@@ -38,3 +38,28 @@ add("Pants", 1);
 console.log(cart);
 // 비록 다른 파일에서 import 해왔음에도 불구하고, 위에 add한 요소가 cart 배열 안에 그대로 다 들어가 있다. 이는 JS가 사본을 복사하는 것이 아니라, Live connection으로 모듈을 가지고 옴을 뜻한다. 화면 뒤에서는 동일한 개체로 작동한다.
 */
+
+/*
+// [17-273] Top-Level Await (ES2022)
+// html script Tag에서 module을 사용하면, Module를 비동기로 불러오는 방식이다보니, 이제는 async 키워드가 없어도 await를 사용할 수 있다. 만약에 Import대상 모듈에서 상위 await문을 사용한다면, 그 작업이 다 끝날 때까지 import를 받을 모듈이 실행되는 것을 막는다. 그래서 차상위 await를 사용할 때는 항상 주의를 하고 사용해야 한다. 안 그러면 프로그램이 매우 느려진다.
+console.log('Start fetching');
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+console.log('Something');
+
+
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// Not very clean - 원래는 await를 async 함수 안에서만 써야했기에 차상위 레벨에서는 then()을 무조건 써야 했다.
+lastPost.then(last => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+*/
